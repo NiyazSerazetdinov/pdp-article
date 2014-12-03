6e705a35324528b49464d5bbd751f1bc8232a081
NiyazSerazetdinov <niyaz.serazetdinov@flatstack.com>
2014-12-03 15:43:37 +0300

    Introduce articles

---
+++ app/models/article.rb
@@ -0,0 +1,14 @@
+class Article < ActiveRecord::Base
+  belongs_to :user
+
+  validates :user, :text, :presence => true
+  validates :title,
+    :presence => true,
+    :length => {
+      :maximum => 60
+    },
+    :uniqueness => {
+      :case_sensitive => false
+    }
+
+end
