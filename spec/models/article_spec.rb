31d2e5491d366bec168baa6e1ecf488bc0102105
Niyaz Serazetdinov <niyaz.serazetdinov@flatsack.com>
2014-12-24 11:29:50 +0300

    add comment and comment_presenter specs

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -7,4 +7,5 @@ describe Article do
   it { is_expected.to validate_presence_of :text }
   it { is_expected.to validate_presence_of :user }
   it { is_expected.to belong_to(:user) }
+  it { is_expected.to have_many(:comments).dependent(:destroy) }
 end
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Comment do
+  it { is_expected.to validate_presence_of :text }
+  it { is_expected.to validate_presence_of :article }
+  it { is_expected.to validate_presence_of :user }
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to belong_to(:article) }
+end
