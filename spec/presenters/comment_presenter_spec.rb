ecf0664a407f8f8991384c6ad91ebe6f90a6890b
Niyaz Serazetdinov <niyaz.serazetdinov@flatsack.com>
2015-01-15 01:46:25 +0300

    extract date to 'let' in specs

--- spec/presenters/comment_presenter_spec.rb
+++ spec/presenters/comment_presenter_spec.rb
@@ -4,11 +4,12 @@ describe CommentPresenter do
   let(:user) { build(:user, full_name: 'User Shmuser') }
   let(:comment) { create(:comment, user: user) }
   let(:presenter) { described_class.wrap(comment) }
+  let(:yesterday) { Date.yesterday }
 
   describe '#comment_info' do
-    before { comment.created_at = Date.yesterday }
+    before { comment.created_at = yesterday }
     subject { presenter.comment_info }
 
-    it { is_expected.to eq "Written Yesterday #{ Date.yesterday.strftime('at %I:%M%p')} by User Shmuser" }
+    it { is_expected.to eq "Written Yesterday #{ yesterday.strftime('at %I:%M%p')} by User Shmuser" }
   end
 end
